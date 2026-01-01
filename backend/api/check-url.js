cddb68ff4e73384648afee3c0fa3e00523629be7
Paulovt17 <paulooteixeira013@gmail.com>
2026-01-01 15:23:32 -0300

    update check-url.js

--- backend/api/check-url.js
+++ backend/api/check-url.js
@@ -1,4 +1,3 @@
-import dotenv from "dotenv";
 import { checkGoogleSafe } from "../services/googleSafe.js";
 import { checkOpenPhish } from "../services/openPhish.js";
 import { checkUrlHaus } from "../services/urlHaus.js";
